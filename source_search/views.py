1abd8904cb72d741194124f53b6944fa6e7cfa17
Erik Osmond <erikosmond@gmail.com>
2014-01-18 15:50:58 -0500

    adding files i forgot to stage last time

--- source_search/views.py
+++ source_search/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.template import loader, RequestContext
+
+from django.shortcuts import redirect, render_to_response
+
+def index(request):
+    return render_to_response("index.html",
+        {'message':'hi'},RequestContext(request))
